8b2aa3034b0b9ad12745617f6d934b22f0366cf2
Student28 <Student28@college.local>
2024-09-17 12:38:06 +0300

    ЛЗ9 Настройка формы и ее полей
    Изменение внешнего вида поля с помощью параметра widget, задание начальных значений полей с помощью свойства initial, задание порядка. Настройки вида формы, настроили отображение полей на форме с помощью методов: as _ tаbе ( ) - отображение полей в виде таблицы; as_ul ( ) - отображение полей в виде списка; as_p ( ) - отображение каждого поля формы в отдельном параграфе (абзаце). Присвоили стили.

--- web_1/hello/firstapp/views.py
+++ web_1/hello/firstapp/views.py
@@ -56,3 +56,12 @@ def index(request):
 def index(request):
  userform = UserForm()
  return render(request, "firstapp/index.html", {"form": userform})
+
+def index(request):
+ userform = UserForm()
+ if request.method == "POST":
+   userform = UserForm(request.POST)
+   if userform.is_valid():
+      name = userform.cleaned_data["name"]
+      return HttpResponse("<h2>Имя введено коррректно –{0}</h2>".format(name))
+ return render(request, "firstapp/index.html", {"form": userform})
