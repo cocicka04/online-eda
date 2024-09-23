9e68c7adb283dadfa9acb37081326d20d02a7a3e
dashapupupu <dididi2037@mail.ru>
2024-09-23 22:27:48 +0300

    ПЗ 7 Работа с объектами модели данных.
    Реализовали получение сведений из бд и сохранение данных введенных пользователем на html страничке затем добавили возможность их там изменять, удалять. Организовали связи между разными таблицами в модели данных(один ко многим, один к одному, многие ко многим)

--- web_1/hello/firstapp/models.py
+++ web_1/hello/firstapp/models.py
@@ -1,8 +1,32 @@
 from django.db import models
+
 class Person(models.Model):
  name = models.CharField(max_length=20)
  age = models.IntegerField()
+ objects = models.Manager()
+ DoesNotExist = models.Manager
 
+class Company(models.Model):
+ name = models.CharField(max_length=30)
 
+class Product(models.Model):
+ company = models.ForeignKey(Company, on_delete=models.CASCADE)
+ name = models.CharField(max_length=30)
+ price = models.IntegerField()
+
+class Course(models.Model):
+ name = models.CharField(max_length=30)
+
+class Student(models.Model):
+ name = models.CharField(max_length=30)
+ courses = models.ManyToManyField(Course)
+
+class User(models.Model):
+ name = models.CharField(max_length=20)
+ 
+class Account(models.Model):
+ login = models.CharField(max_length=20)
+ password = models.CharField(max_length=20)
+ user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
 
 # Create your models here.
--- web_1/hello/firstapp/views.py
+++ web_1/hello/firstapp/views.py
@@ -1,6 +1,7 @@
 from .forms import UserForm
 from django.shortcuts import render
 from django.http import *
+from .models import Person
 
 
 def index(request):
@@ -65,3 +66,37 @@ def index(request):
       name = userform.cleaned_data["name"]
       return HttpResponse("<h2>Имя введено коррректно –{0}</h2>".format(name))
  return render(request, "firstapp/index.html", {"form": userform})
+
+def index(request):
+ people = Person.objects.all()
+ return render(request, "index.html", {"people": people})
+
+def create(request):
+ if request.method == "POST":
+  klient = Person()
+  klient.name = request.POST.get("name")
+  klient.age = request.POST.get("age")
+  klient.save()
+ return HttpResponseRedirect("/")
+
+def edit(request, id):
+ try:
+  person = Person.objects.get(id=id)
+
+  if request.method == "POST":
+   person.name = request.POST.get("name")
+   person.age = request.POST.get("age")
+   person.save()
+   return HttpResponseRedirect("/")
+  else:
+   return render(request, "edit.html", {"person": person})
+ except Person.DoesNotExist:
+   return HttpResponseNotFound("<h2>Клиент не найден</h2>")
+ 
+def delete(request, id):
+ try:
+   person = Person.objects.get(id=id)
+   person.delete()
+   return HttpResponseRedirect("/")
+ except Person.DoesNotExist:
+   return HttpResponseNotFound("<h2>Клиент не найден</h2>")
